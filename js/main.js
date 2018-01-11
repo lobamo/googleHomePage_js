5ba2c7f0bfce33f9fcf5b3616970b9645e8cabf0
lobamos <namosdjoro@gmail.com>
2018-01-11 22:06:08 +0100

    envoie des fichiers

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function display(){
+	var x = document.getElementById('container');
+	if(x.style.display === 'none') {
+		x.style.display = 'block';
+	}else{
+		x.style.display = "none";
+	}
+}
+
+function hide(){
+	var x = document.getElementById('buttonToHide');
+	if (x.style.display === "block"){
+		x.style.display = 'none';
+	}else{
+		x.style.display = 'block';
+	}
+}
+
+var popup = setTimeout(function(){
+
+alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");},10000);
